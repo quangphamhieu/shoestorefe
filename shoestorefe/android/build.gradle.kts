89a8f800db7bad9c8c7cc364052787af6b0af5ff
quangphamhieu <hieubon211@gmail.com>
2025-12-15 17:30:57 +0700

    update ci\cd 4

--- shoestorefe/android/build.gradle.kts
+++ shoestorefe/android/build.gradle.kts
@@ -11,6 +11,14 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    project.configurations.all {
+        resolutionStrategy {
+            force("androidx.browser:browser:1.8.0")
+            force("androidx.core:core:1.13.1")
+            force("androidx.core:core-ktx:1.13.1")
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
